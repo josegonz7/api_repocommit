8c298a2413453b114dce622f549c001e71fe3cee
josegonz7 <jgoncre1412@g.educaand.es>
2026-03-02 10:08:38 +0100

    services hecho

--- src/main/java/ies_alcores/api_repo_commit/service/CommitsService.java
+++ src/main/java/ies_alcores/api_repo_commit/service/CommitsService.java
@@ -1,4 +1,20 @@
 package ies_alcores.api_repo_commit.service;
 
+
+import ies_alcores.api_repo_commit.model.Commits;
+import ies_alcores.api_repo_commit.repository.CommitsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CommitsService {
+
+    @Autowired
+    private CommitsRepository commitRepository;
+
+    public List<Commits> findAll(){
+        return this.commitRepository.findAll();
+    }
 }
--- src/main/java/ies_alcores/api_repo_commit/service/RepositoriosService.java
+++ src/main/java/ies_alcores/api_repo_commit/service/RepositoriosService.java
@@ -1,4 +1,18 @@
 package ies_alcores.api_repo_commit.service;
 
+import ies_alcores.api_repo_commit.model.Repositorios;
+import ies_alcores.api_repo_commit.repository.RepositoriosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class RepositoriosService {
+
+    @Autowired
+    private RepositoriosRepository repositorioRepository;
+
+    public List<Repositorios> findAll(){
+        return this.repositorioRepository.findAll();
+    }
 }
