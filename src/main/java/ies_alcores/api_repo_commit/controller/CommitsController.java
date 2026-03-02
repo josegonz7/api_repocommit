5fe1d9d264c4dd1eddfb9520ac70618ad83e4316
josegonz7 <jgoncre1412@g.educaand.es>
2026-03-02 10:15:43 +0100

    controller hecho

--- src/main/java/ies_alcores/api_repo_commit/controller/CommitsController.java
+++ src/main/java/ies_alcores/api_repo_commit/controller/CommitsController.java
@@ -1,4 +1,23 @@
 package ies_alcores.api_repo_commit.controller;
 
+import ies_alcores.api_repo_commit.model.Commits;
+import ies_alcores.api_repo_commit.service.CommitsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/commits")
 public class CommitsController {
+
+    @Autowired
+    private CommitsService commitService;
+
+    @GetMapping
+    public List<Commits> findAll(){
+        return this.commitService.findAll();
+    }
 }
--- src/main/java/ies_alcores/api_repo_commit/controller/RepositoriosController.java
+++ src/main/java/ies_alcores/api_repo_commit/controller/RepositoriosController.java
@@ -1,4 +1,23 @@
 package ies_alcores.api_repo_commit.controller;
 
+import ies_alcores.api_repo_commit.model.Repositorios;
+import ies_alcores.api_repo_commit.service.RepositoriosService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/repositorios")
 public class RepositoriosController {
+
+    @Autowired
+    private RepositoriosService repositorioService;
+
+    @GetMapping
+    public List<Repositorios> findAll(){
+        return this.repositorioService.findAll();
+    }
 }
