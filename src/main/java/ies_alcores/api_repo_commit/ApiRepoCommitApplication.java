d4fac6aa3b0c11bda570e7d57cbb54412c819497
josegonz7 <jgoncre1412@g.educaand.es>
2026-03-02 09:08:03 +0100

    Initial commit

---
+++ src/main/java/ies_alcores/api_repo_commit/ApiRepoCommitApplication.java
@@ -0,0 +1,13 @@
+package ies_alcores.api_repo_commit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRepoCommitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRepoCommitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ies_alcores/api_repo_commit/ApiRepoCommitApplicationTests.java
@@ -0,0 +1,13 @@
+package ies_alcores.api_repo_commit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRepoCommitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
