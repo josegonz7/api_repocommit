eabfb457407d05ac1778a1fb21aeafd6c9a11bda
josegonz7 <jgoncre1412@g.educaand.es>
2026-03-05 14:22:53 +0100

    ultimos cambios

--- src/main/java/ies_alcores/api_repo_commit/controller/CommitsController.java
+++ src/main/java/ies_alcores/api_repo_commit/controller/CommitsController.java
@@ -15,31 +15,26 @@ public class CommitsController {
     @Autowired
     private CommitsRepository repository;
 
-    // 1. POST - Ahora se llamará "/crear-nuevo"
     @PostMapping("/crear_nuevo")
     public Commits guardar(@RequestBody Commits nuevoCommit) {
         return repository.save(nuevoCommit);
     }
 
-    // 2. CONSULTA 1 - Buscar por mensaje: "/buscar-por-texto/{texto}"
     @GetMapping("/buscar_por_texto/{texto}")
     public List<Commits> buscarMensaje(@PathVariable String texto) {
         return repository.buscarPorMensaje(texto);
     }
 
-    // 3. CONSULTA 2 - Por nombre de repo: "/ver-del-repositorio/{nombre}"
     @GetMapping("/ver_del_repositorio/{nombre}")
     public List<Commits> buscarRepo(@PathVariable String nombre) {
         return repository.buscarPorNombreRepo(nombre);
     }
 
-    // 4. CONSULTA 3 - Conteo: "/estadisticas/total-en-repo/{id}"
     @GetMapping("/estadisticas/total-en-repo/{id}")
     public Long contar(@PathVariable Long id) {
         return repository.contarCommitsPorRepo(id);
     }
 
-    // 5. DELETE - Borrar: "/eliminar-por-hash/{hash}"
     @DeleteMapping("/eliminar_por_hash/{hash}")
     public ResponseEntity<String> borrar(@PathVariable String hash) {
         repository.borrarPorHash(hash);
--- src/main/java/ies_alcores/api_repo_commit/repository/CommitsRepository.java
+++ src/main/java/ies_alcores/api_repo_commit/repository/CommitsRepository.java
@@ -12,19 +12,15 @@ import java.util.List;
 @Repository
 public interface CommitsRepository extends JpaRepository<Commits, Long> {
 
-    // CONSULTA 1: Buscar commits por palabra clave en el mensaje
     @Query("SELECT c FROM Commits c WHERE c.mensaje LIKE %:palabra%")
     List<Commits> buscarPorMensaje(String palabra);
 
-    // CONSULTA 2: Buscar todos los commits de un repositorio por su NOMBRE
     @Query("SELECT c FROM Commits c WHERE c.repositorios.nombre = :nombreRepo")
     List<Commits> buscarPorNombreRepo(String nombreRepo);
 
-    // CONSULTA 3: Contar cuántos commits tiene un repositorio por su ID
     @Query("SELECT COUNT(c) FROM Commits c WHERE c.repositorios.id_repo = :idRepo")
     Long contarCommitsPorRepo(Long idRepo);
 
-    // DELETE con @Query: Borrar un commit por su HASH
     @Transactional
     @Modifying
     @Query("DELETE FROM Commits c WHERE c.hash = :hash")
